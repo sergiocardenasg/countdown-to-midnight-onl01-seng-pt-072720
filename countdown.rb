49cf13b4969eb6666a55595cc63f03705c3ee41d
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-07-29 01:41:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,14 +3,15 @@ def countdown (integer)
     puts "#{integer} SECOND(S)!"
     integer -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep (integer, slp)
-  until integer == 0
-    integer -= 1
-    puts "#{integer} SECOND(S)!"
-    sleep slp
-  end
-  puts "HAPPY NEW YEAR!"
-end
+#def countdown_with_sleep (integer, slp)
+#  until integer == 0
+#    integer -= 1
+#    puts "#{integer} SECOND(S)!"
+#    sleep slp
+#  end
+#  puts "HAPPY NEW YEAR!"
+#end
+
