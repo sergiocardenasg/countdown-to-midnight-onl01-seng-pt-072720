bda6f7ff289f6e0e3a28f69c05b2dcaf5d4170b6
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-07-29 01:42:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,11 +6,11 @@ def countdown (integer)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep (integer, slp)
+def countdown_with_sleep (integer)
   until integer == 0
     integer -= 1
     puts "#{integer} SECOND(S)!"
-    sleep slp
+    sleep 1
   end
   return "HAPPY NEW YEAR!"
 end
