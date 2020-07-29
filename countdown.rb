75b5d20916b29d06f7bcbb64b2064386e6f83ccd
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-07-29 01:43:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown (integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (integer)
+  until integer == 0
+    integer -= 1
+    puts "#{integer} SECOND(S)!"
+    sleep 1
+  end
 end
